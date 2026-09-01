64a448c1080db472f77ab16718c95b3edb5beb26
John <you@example.com>
2026-08-31 23:07:04 -0400

    x-rewards-extension/README.md

--- Momo rewards/content.js
+++ Momo rewards/content.js
@@ -151,7 +151,7 @@ function showToast(message, success) {
     const toast = document.createElement("div");
     toast.textContent = message;
     toast.style.position = "fixed";
-    toast.style.top = "90px";
+    toast.style.top = "20px";
     toast.style.left = "50%";
     toast.style.transform = "translateX(-50%)";
     toast.style.zIndex = "999999";
